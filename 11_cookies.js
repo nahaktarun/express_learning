cca58a88ff3081cec79f39fd23f9277c3c4e8af5
Tarun Nahak <tarunnahak25@gmail.com>
2023-03-25 12:32:13 +0530

    fixed cookie fetch

--- 11_cookies.js
+++ 11_cookies.js
@@ -6,8 +6,10 @@
 var cookieParser = require("cookie-parser");
 var express = require("express");
 var app = express();
+app.use(cookieParser());
 app.get("/", (req, res) => {
-  res.cookie("name", "Tarun", { maxAge: 25000 }).send("cookie set");
+  res.cookie("name", "Tarun", { maxAge: 25000 }).send(req.cookies);
+  console.log(req.cookies["name"]);
 });
 
 app.get("/clear_cookie", (req, res) => {
