75afffc1b7f56ccbcde7e0561fbf0614447e6718
Tarun Nahak <tarunnahak25@gmail.com>
2023-04-17 18:59:04 +0530

    session 15 process

---
+++ 15_process.js
@@ -0,0 +1,34 @@
+// What is process?
+// each process is isoloted from each other
+
+// process1
+//  process2 : running instance of a computer program
+// process module
+// process events:
+
+// 1. exit
+// 2. beforeExit
+// 3. uncaughtException
+// 4. Signal events => signin
+
+// monolith and microservice:
+
+// process.on("exit", (statuscode) => {
+//   setTimeout(() => {
+//     console.log("code exited");
+//   }, 0);
+//   console.log("Code exit with code ", statuscode);
+// });
+// console.log("Program");
+
+// using process write on tothe console
+// process.stdout.write("Hello world" + "\n");
+
+// getting console inputs
+// process.argv.forEach((value, index, array) => {
+//   console.log(index, value, array);
+// });
+
+console.log(process.memoryUsage());
+
+console.log(process.version);
